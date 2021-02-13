5d0973885871f5f03cb77a221fc237bf39361e27
Travis Reed <traviswreed2@gmail.com>
2021-02-13 11:49:22 -0600

    Week 3 workshop

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -36,6 +36,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+    const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -70,6 +71,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         },
