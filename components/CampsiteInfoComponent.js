58a2868d6375e97f2a3a3c5b7f9d387599b5e6c6
Travis Reed <traviswreed2@gmail.com>
2021-02-19 18:54:35 -0600

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -9,6 +9,7 @@ import {
     StyleSheet,
     Alert,
     PanResponder,
+    Share,
 } from "react-native";
 import * as Animatable from "react-native-animatable";
 import { Card, Icon, Rating, Input } from "react-native-elements";
@@ -22,6 +23,19 @@ const mapStateToProps = (state) => {
         comments: state.comments,
         favorites: state.favorites,
     };
+};
+
+const shareCampsite = (title, message, url) => {
+    Share.share(
+        {
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url,
+        },
+        {
+            dialogTitle: "Share " + title,
+        }
+    );
 };
 
 const mapDispatchToProps = {
@@ -110,6 +124,20 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={"share"}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
