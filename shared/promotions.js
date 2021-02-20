bd56ae2743aa74a13896acfb12d25a9f76880333
Travis Reed <traviswreed2@gmail.com>
2021-02-19 18:24:47 -0600

    test

--- shared/promotions.js
+++ shared/promotions.js
@@ -4,6 +4,42 @@ export const PROMOTIONS = [
         name: "Mountain Adventure",
         image: "/assets/images/breadcrumb-trail.jpg",
         featured: true,
-        description: "Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided."
+        description:
+            "Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided.",
+    },
+];
+
+class Main extends Component {
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
     }
-]
+    render() {
+        const HomePage = () => {
+            return (
+                <Home
+                    campsite={
+                        this.props.campsites.campsites.filter(
+                            (campsite) => campsite.featured
+                        )[0]
+                    }
+                    campsitesLoading={this.props.campsites.isLoading}
+                    campsitesErrMess={this.props.campsites.errMess}
+                    promotion={
+                        this.props.promotions.promotions.filter(
+                            (promotion) => promotion.featured
+                        )[0]
+                    }
+                    promotionLoading={this.props.promotions.isLoading}
+                    promotionErrMess={this.props.promotions.errMess}
+                    partner={
+                        this.props.partners.filter(
+                            (partner) => partner.featured
+                        )[0]
+                    }
+                />
+            );
+        };
+    }
+}
